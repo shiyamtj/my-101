2e623802dd594bbcb09efd470899a111a07bfdda
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-03 05:41:06 +1300

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -83,7 +83,7 @@ const config: Config = {
         },
         // { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/shiyamtj/my-101',
           label: 'GitHub',
           position: 'right',
         },
