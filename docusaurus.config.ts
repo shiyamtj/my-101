2f3ea6e36da05d073e832f677034d4b7936f3e93
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-03 05:15:56 +1300

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://shiyamtj.github.io/my-101/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         // {
